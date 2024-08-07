0ac2a2f408e16a2f81a5a12cdc78825f2c0bae8d
SEYOUNG PARK <seyeongb18@gmail.com>
2024-08-07 20:08:02 +0900

    feat: actionBar title 변경, 액션바 색상 변경, 서치뷰 메뉴 등록

--- Ch11_JetPack/src/main/java/com/example/ch11_jetpack/MainActivity.kt
+++ Ch11_JetPack/src/main/java/com/example/ch11_jetpack/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.ch11_jetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean { // 액티비티가 실행되면서 처음에 한 번만 호출됨
+        menuInflater.inflate(R.menu.menu_main, menu)
+        val menuItem = menu?.findItem(R.id.menu_search)
+        val searchView = menuItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // 키보드의 검색 버튼을 클릭한 순간의 이벤트
+//                Log.d("searchView",query)
+                return true
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // 검색어 변경 이벤트
+                return true
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
     }
 }
