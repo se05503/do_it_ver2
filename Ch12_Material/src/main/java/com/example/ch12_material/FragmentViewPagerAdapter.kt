1bcb788f4dfece9a86b94de7d5432a1ff6052804
SEYOUNG PARK <seyeongb18@gmail.com>
2024-08-10 12:10:40 +0900

    add: 모듈 생성 및 이전 모듈 내용 복사 content: chap12 실습을 위한 모듈을 새로 생성하고 chap11 실습 과정에서 생성한 모듈 내용을 복사했습니다.
    branch를 사용하면 일일히 복사할 필요가 없지만, 책에서는 모듈을 생성하는 방향으로 나와있어서 모듈 내용에 대한 복사는 따로 추가적으로 공부를 해야합니다.

---
+++ Ch12_Material/src/main/java/com/example/ch12_material/FragmentViewPagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.ch12_material
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class FragmentViewPagerAdapter(activity: FragmentActivity): FragmentStateAdapter(activity) {
+    val fragments: List<Fragment>
+    init {
+        fragments = listOf(OneFragment(), TwoFragment(), ThreeFragment())
+    }
+
+    // 프래그먼트 항목 개수 반환
+    override fun getItemCount(): Int = fragments.size
+
+    // 항목을 구성하는 프래그먼트 객체 반환
+    override fun createFragment(position: Int): Fragment = fragments[position]
+}
