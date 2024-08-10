e316f0766784526f68e4be77f8b7845eb7c5d55a
SEYOUNG PARK <seyeongb18@gmail.com>
2024-08-10 12:10:40 +0900

    add: 모듈 생성 및 이전 모듈 내용 복사 content: chap12 실습을 위한 모듈을 새로 생성하고 chap11 실습 과정에서 생성한 모듈 내용을 복사했습니다.
    branch를 사용하면 일일히 복사할 필요가 없지만, 책에서는 모듈을 생성하는 방향으로 나와있어서 모듈 내용에 대한 복사는 따로 추가적으로 공부를 해야합니다.

---
+++ Ch12_Material/src/main/java/com/example/ch12_material/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.ch12_material
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.ch11_jetpack.databinding.ActivityMainBinding
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    val fragmentManager: FragmentManager = supportFragmentManager
+    val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+    val oneFragment = OneFragment()
+    lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // SecurityException resolve
+        val dexOutputDir: File = codeCacheDir
+        dexOutputDir.setReadOnly()
+
+        // viewpager2
+        binding.viewpager.adapter = FragmentViewPagerAdapter(this@MainActivity)
+
+        // drawerlayout
+        toggle = ActionBarDrawerToggle(
+            this@MainActivity,
+            binding.drawer,
+            R.string.drawer_opened,
+            R.string.drawer_closed
+        )
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 토글 버튼으로 사용할 아이콘 출력
+        toggle.syncState() // 왼쪽 화살표 모양 대신 내비게이션 아이콘 출력
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean { // 액티비티가 실행되면서 처음에 한 번만 호출됨
+        menuInflater.inflate(R.menu.menu_main, menu)
+        val menuItem = menu?.findItem(R.id.menu_search)
+        val searchView = menuItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // 키보드의 검색 버튼을 클릭한 순간의 이벤트
+                Log.d("search result : ", query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // 검색어 변경 이벤트
+                return true
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // 이벤트가 토글 버튼에서 발생하는 경우
+        if(toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ Ch12_Material/src/main/java/com/example/ch12_material/OneFragment.kt
@@ -0,0 +1,112 @@
+package com.example.ch12_material
+
+import android.content.Context
+import android.graphics.*
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.ViewCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ch11_jetpack.databinding.FragmentOneBinding
+import com.example.ch11_jetpack.databinding.ItemMainBinding
+
+class MyViewHolder(val binding: ItemMainBinding) : RecyclerView.ViewHolder(binding.root)
+
+class MyAdapter(val datas: MutableList<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    // 항목의 뷰를 가지는 뷰 홀더 객체 준비, 반환된 뷰 홀더 객체는 onBindViewHolder 함수의 매개변수로 전달됨
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        MyViewHolder(ItemMainBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        // 뷰 홀더의 데이터 출력
+        val binding = (holder as MyViewHolder).binding
+        binding.tvItem.text = datas[position]
+        binding.itemRoot.setOnClickListener {
+            Log.d("item number : ", "$position")
+        }
+    }
+
+    // 항목 개수를 판단, 이 함수가 반환한 숫자만큼 onBindViewHolder 함수가 호출됨
+    override fun getItemCount(): Int = datas.size
+
+}
+
+class RecyclerViewDecoration(val context: Context) : RecyclerView.ItemDecoration() {
+
+    // 항목이 배치되기 전에 호출
+    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDraw(c, parent, state)
+        // recyclerview 의 background image 지정
+//        c.drawBitmap(BitmapFactory.decodeResource(context.resources, R.drawable.kbo), 0f, 0f, null)
+    }
+
+    // 항목이 모두 배치된 후 호출
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDrawOver(c, parent, state)
+
+        // 뷰 크기 계산
+        val width = parent.width
+        val height = parent.height
+
+        // 이미지 크기 계산
+        val dr: Drawable? = ResourcesCompat.getDrawable(context.resources, R.drawable.kbo, null)
+        val drWidth = dr?.intrinsicWidth
+        val drHeight = dr?.intrinsicHeight
+
+        // 이미지가 그려질 위치 계산
+        val left = width / 2 - drWidth?.div(2) as Int
+        val top = height / 2 - drHeight?.div(2) as Int
+
+        c.drawBitmap(
+            BitmapFactory.decodeResource(context.resources, R.drawable.kbo),
+            left.toFloat(),
+            top.toFloat(),
+            null
+        )
+    }
+
+    // 개별 항목 꾸밀 때 호출 → 각 항목의 여백 설정 및 바탕색 설정
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        val index = parent.getChildAdapterPosition(view) + 1
+        if (index % 3 == 0) {
+            // index 가 3, 6, 9 ... 인 경우 → 3번째, 6번째, 9번째 항목 ( 1을 더했음 )
+            outRect.set(10, 10, 10, 150)
+        } else {
+            outRect.set(10, 10, 10, 0)
+        }
+        view.setBackgroundColor(Color.CYAN)
+        ViewCompat.setElevation(view, 20.0f)
+    }
+}
+
+class OneFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentOneBinding.inflate(inflater, container, false)
+        // 여기 사이에 작성하면 된다 (실습 책 내용 기준)
+        val datas = mutableListOf<String>()
+        for (i in 1..10) {
+            datas.add("Item $i")
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(activity) // 매개변수 = context
+        binding.recyclerView.adapter = MyAdapter(datas)
+        binding.recyclerView.addItemDecoration(RecyclerViewDecoration(activity as Context)) // 매개변수 = context
+        return binding.root
+    }
+}
