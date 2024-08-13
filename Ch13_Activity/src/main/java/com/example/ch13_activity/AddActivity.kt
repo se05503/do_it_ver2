355bf225cb123f96b78b01031165c4bfe9a09d1b
SEYOUNG PARK <seyeongb18@gmail.com>
2024-08-13 20:01:27 +0900

    feat, design: 액티비티간 화면 이동 및 레이아웃 구성
    EditText 의 margin 값을 이해하지 못했습니다. (나중에 해설보고 리팩토링 하기)

--- Ch13_Activity/src/main/java/com/example/ch13_activity/AddActivity.kt
+++ Ch13_Activity/src/main/java/com/example/ch13_activity/AddActivity.kt
@@ -1,11 +1,15 @@
 package com.example.ch13_activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ch13_activity.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add)
+        val binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
--- Ch13_Activity/src/main/java/com/example/ch13_activity/MainActivity.kt
+++ Ch13_Activity/src/main/java/com/example/ch13_activity/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.ch13_activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ch13_activity.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // SecurityException resolve
+        val dexOutputDir: File = codeCacheDir
+        dexOutputDir.setReadOnly()
+
+        binding.extendedFab.setOnClickListener {
+            // 할 일 등록 화면(AddActivity)로 이동 → intent 사용
+            val intent: Intent = Intent(this@MainActivity, AddActivity::class.java) // 컴포넌트의 정보를 intent에 담음, 클래스 타입 레퍼런스 정보
+            startActivity(intent) // 해당 함수가 intent 를 시스템에 전달
+        }
     }
 }
