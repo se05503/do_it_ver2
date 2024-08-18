f4f21c8e20b470f8ddce6a35007a4d286aa6bb37
SEYOUNG PARK <seyeongb18@gmail.com>
2024-08-18 20:21:03 +0900

    feat : ActivityResultLauncher 사용
    로그를 찍어봤는데 데이터가 전달되지 않는다.
    코드를 수정하는 과정을 기록하기 위해 1차 commit 을 한다.

--- Ch13_Activity/src/main/java/com/example/ch13_activity/AddActivity.kt
+++ Ch13_Activity/src/main/java/com/example/ch13_activity/AddActivity.kt
@@ -3,14 +3,18 @@ package com.example.ch13_activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.example.ch13_activity.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAddBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityAddBinding.inflate(layoutInflater)
+        binding = ActivityAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // actionbar 의 내용을 toolbar 에 적용
@@ -25,7 +29,11 @@ class AddActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
         R.id.save -> {
             // MainActivity 로 돌아가기 + 기능 추가
-            finish()
+//            finish()
+            Log.d("왜 안보내져: ", binding.etTodo.text.toString())
+            val intent = Intent(this@AddActivity, MainActivity::class.java)
+            intent.putExtra("Todo", binding.etTodo.text.toString())
+            startActivity(intent)
             true
         }
         else -> super.onOptionsItemSelected(item)
--- Ch13_Activity/src/main/java/com/example/ch13_activity/MainActivity.kt
+++ Ch13_Activity/src/main/java/com/example/ch13_activity/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.ch13_activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.ch13_activity.databinding.ActivityMainBinding
 import java.io.File
 
@@ -19,10 +22,22 @@ class MainActivity : AppCompatActivity() {
         // actionbar 내용을 toolbar 에 적용
         setSupportActionBar(binding.toolbar)
 
+        // ActivityResultLauncher
+        val requestLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+            // Contract 객체 (실제 작업자)
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            // Callback 객체 (결과 처리)
+            val todoList = it.data?.getStringExtra("Todo")
+            Log.d("왜 안왔지?", todoList!!)
+            binding.tvTemp.text = "result: $todoList"
+        }
+
         binding.extendedFab.setOnClickListener {
             // 할 일 등록 화면(AddActivity)로 이동 → intent 사용
-            val intent: Intent = Intent(this@MainActivity, AddActivity::class.java) // 컴포넌트의 정보를 intent에 담음, 클래스 타입 레퍼런스 정보
-            startActivity(intent) // 해당 함수가 intent 를 시스템에 전달
+            val intent = Intent(this@MainActivity, AddActivity::class.java) // 컴포넌트의 정보를 intent에 담음, 클래스 타입 레퍼런스 정보
+            requestLauncher.launch(intent) // 해당 함수가 intent 를 시스템에 전달
         }
+
     }
 }
