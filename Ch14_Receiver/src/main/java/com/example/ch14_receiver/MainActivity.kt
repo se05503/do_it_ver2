375bcec67cf04e87ba9785011bab4441d6daa170
SEYOUNG PARK <seyeongb18@gmail.com>
2024-08-22 12:33:10 +0900

    feat: 버튼 눌렀을 때 브로드캐스트 리시버 실행하기(알림X) sendBroadcast 함수 사용
    * 나중에 리팩토링 할점(개선사항)
    - 배터리 상태가 바뀌지 않는 경우 : 실시간, 뒤로가기로 앱 나가기
    - 배터리 상태가 바뀌는 경우 : 앱을 아예 종료하고 다시 들어가기

--- Ch14_Receiver/src/main/java/com/example/ch14_receiver/MainActivity.kt
+++ Ch14_Receiver/src/main/java/com/example/ch14_receiver/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ch14_receiver.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +18,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // SecurityException resolve
+        val dexOutputDir: File = codeCacheDir
+        dexOutputDir.setReadOnly()
+
         // broadcast receiver
         val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
         val batteryStatus = registerReceiver(null, intentFilter)
@@ -25,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         val level = batteryStatus!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
         val scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
         val batteryPct = level/scale.toFloat() * 100
-        binding.tvBatteryPercentage.text = "$batteryPct %"
+        binding.tvBatteryPercentage.text = "$batteryPct%"
 
         // 사용자 기기에 전원이 공급되는 지 알아보는 경우
         val status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
@@ -47,8 +52,14 @@ class MainActivity : AppCompatActivity() {
         } else {
             // 전원이 공급되지 않는 상태
             binding.tvPluginStatus.text = "Not Plugged"
+            binding.ivPluginStatus.setImageResource(R.drawable.ic_disconnect)
         }
 
-
+        // 버튼을 누르면 브로드캐스트 리시버를 실행시켜 알림을 띄운다.
+        binding.btnRunReceiver.setOnClickListener {
+            // 액티비티 컴포넌트에서 인텐트를 시스템에 전달하여 브로드캐스트 리시버를 실행해달라고 요청함
+            val intent = Intent(this@MainActivity, MyReceiver::class.java)
+            sendBroadcast(intent)
+        }
     }
 }
--- Ch14_Receiver/src/main/java/com/example/ch14_receiver/MyReceiver.kt
+++ Ch14_Receiver/src/main/java/com/example/ch14_receiver/MyReceiver.kt
@@ -7,7 +7,9 @@ import android.content.Intent
 class MyReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("MyReceiver.onReceive() is not implemented")
+        // 컴포넌트에서 리시버를 실행하려고 인텐트를 시스템에 전달하면 해당 함수가 자동으로 호출된다.
+        // 두번째 매개변수: 자신을 호출한 인텐트 객체
+        // onReceive() 함수는 실행한 후 10초 이내에 완료할 것을 권장하므로 오래 걸리는 작업을 담기에는 부적절하다.
+        // 알림 띄우는 코드 작성하기
     }
 }
