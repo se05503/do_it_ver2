61a48ba20f47bcd5a0c9b9052bfcd21aca127c41
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-11 20:49:57 +0900

    [feat] : API에서 제공하는 Messenger 객체 이용 (방법2) - API에서 제공하는 Messenger 객체 바인딩 - Messenger 객체를 이용하는 방법은 IPC(inter-process communication) 할 때 사용할 수 있다. - 외부 앱과 연동하여 프로세스끼리 통신할 때도 사용한다. - 프로세스간 통신 방법(IPC): 1) AIDL 2) Messenger -> AIDL 보다 간단하다

--- Ch15_Outer/src/main/java/com/example/ch15_outer/MessengerService.kt
+++ Ch15_Outer/src/main/java/com/example/ch15_outer/MessengerService.kt
@@ -9,16 +9,28 @@ import android.widget.Toast
 
 class MessengerService : Service() {
 
-    class MessengerBinder : Binder() {
-        fun temp(num: Int):Int {
-            return num*num
+    lateinit var messenger: Messenger
+    internal class IncomingHandler(
+        context: Context,
+        private val applicationContext: Context = context.applicationContext
+    ): Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            /*
+            외부에서 서비스에 데이터를 전달할 때 자동으로 호출됨
+            parameter(msg) = 외부에서 전달받은 데이터
+            msg.what = 어떤 성격의 데이터인지 구분
+            msg.obj = 전달된 데이터
+            */
+            when(msg.what) {
+                10 -> Toast.makeText(applicationContext, "${msg.obj}", Toast.LENGTH_LONG).show()
+                20 -> Toast.makeText(applicationContext, "${msg.obj}", Toast.LENGTH_LONG).show()
+                else -> super.handleMessage(msg)
+            }
         }
     }
 
-    override fun onBind(intent: Intent): IBinder {
-        // Return the communication channel to the service.
-        // bindService() 함수로 서비스를 실행하면 호출됨
-        // 서비스를 실행한 곳에 IBinder 인터페이스를 구현한 객체 바인딩(전달)
-        return MessengerBinder()
+    override fun onBind(intent: Intent): IBinder? {
+        messenger = Messenger(IncomingHandler(this))
+        return messenger.binder
     }
 }
--- Ch15_Service/src/main/java/com/example/ch15_service/MainActivity.kt
+++ Ch15_Service/src/main/java/com/example/ch15_service/MainActivity.kt
@@ -7,59 +7,48 @@ import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
+import android.os.Message
+import android.os.Messenger
 import com.example.ch15_outer.MessengerService
 import com.example.ch15_service.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var messenger: Messenger
+
+    val connection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            // bindService() 함수로 서비스를 실행할 때 자동으로 호출됨
+            // 두번째 파라미터(service): 서비스에서 넘어온 객체
+            messenger = Messenger(service)
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            // unbindService() 함수로 서비스를 종료할 때 자동으로 호출됨
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*
-        외부 앱(outer)이 백그라운드 상태이면 서비스를 실행할 수 없음
-        method 1) startService()로 서비스 실행
-        서비스 종료: stopService(intent)
-
         val intent = Intent("ACTION_SERVICE_Messenger")
         intent.setPackage("com.example.ch15_outer")
-        startService(intent)
-         */
-
-        /*
-        method 2 - A) bindService() 함수로 실행
-        third parameter : Int type flags 으로 대부분 Context.BIND_AUTO_CREATE 를 사용
-        Context.BIND_AUTO_CREATE : 서비스가 실행 상태가 아니더라도 객체를 생성해서 실행하라는 의미
-        만약 해당 flags 로 지정하지 않으면 인텐트를 시스템에 전달해도 서비스가 실행 상태가 아니면 동작하지 않음
-        unbindService(connection) -> 서비스 종료
-        */
-        val connection: ServiceConnection = object : ServiceConnection {
-            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-                // bindService() 함수로 서비스를 실행할 때 자동으로 호출됨
-                // 두번째 파라미터(service): 서비스의 onBind() 함수에서 바인딩된 Binder 객체
-                val serviceBinder = service as MessengerService.MessengerBinder
-                serviceBinder.temp(20)
-            }
-
-            override fun onServiceDisconnected(name: ComponentName?) {
-                // unbindService() 함수로 서비스를 종료할 때 자동으로 호출됨
-            }
-        }
-
-        /*
-        method 2 - B) API에서 제공하는 Messenger 객체 바인딩
-        Messenger 객체를 이용하는 방법은 IPC(inter-process communication) 할 때 사용할 수 있다.
-        외부 앱과 연동하여 프로세스끼리 통신할 때도 사용한다.
-        프로세스간 통신 방법: 1) AIDL 2) Messenger -> AIDL 보다 간단한 코드
-         */
-
-        val intent = Intent("ACTION_SERVICE_Messenger")
-        intent.setPackage("com.example.ch15_outer")
-
         bindService(intent, connection, Context.BIND_AUTO_CREATE)
+
+        // 서비스 실행 후 서비스에 데이터를 전달하고 싶은 경우 -> send 함수를 호출하는 순간 서비스의 handleMessage 가 자동 호출됨
+        // IPC 통신에서는 주고받는 데이터는 Parcelable 이나 Bundle 타입이어야 한다.
+        // 데이터를 먼저 Bundle에 담고 다시 Message 객체에 담아서 전달해야 한다.
+        val bundle = Bundle()
+        bundle.putString("string_key", "value1")
+        bundle.putInt("int_key", 10)
+
+        val msg = Message()
+        msg.what = 10
+        msg.obj = bundle
+        messenger.send(msg)
 
         binding.messengerPlayStart.setOnClickListener {
 
